14e393406fc08a77c4fe459a9ead2ab1db525707
Akash Limkar <akashlimkar.111999@gmail.com>
2023-11-11 16:26:58 +0530

    Update inventory_update.py

--- inventory_update.py
+++ inventory_update.py
@@ -1,18 +1,32 @@
 def update_inventory(arr1, arr2):
-    
+    # Combine the arrays
     combined_arr = arr1 + arr2
 
-    inventory_dict = {}
+    # Use a regular dictionary to accumulate quantities by item name
+    #item_counts = {}
+    #for quantity, item in combined_arr:
+    #   item_counts[item] = item_counts.get(item, 0) + quantity
+        
+    item_counts = {}
+    for quantity, item in combined_arr:
+        if item in item_counts:
+            item_counts[item] = item_counts[item] + quantity
+        else:
+            item_counts[item] = quantity
 
-    # Populate the dictionary with quantities
-    for item in combined_arr:
-        quantity = inventory_dict.get(item[1], 0)
-        inventory_dict[item[1]] = quantity + item[0]
+    # Convert dictionary items to a list of lists
+    #new_inventory = []
+    #for item, quantity in item_counts.items():
+    #    if quantity != 0:
+    #        new_inventory.append([quantity, item])
+            
+    new_inventory = []
+    for item in item_counts:
+        quantity = item_counts[item]
+        if quantity != 0:
+            new_inventory.append([quantity, item])
 
-    # dictionary to list
-    new_inventory = [[quantity, item] for item, quantity in inventory_dict.items() if quantity != 0]
-
-    # Sort
+    # Sort the list based on item names
     new_inventory.sort(key=lambda x: x[1])
 
     return new_inventory
